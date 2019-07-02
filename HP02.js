7c873855fa000bf4acf756bb43cf697a95d3a7ad
rasucal <52271061+rasucal@users.noreply.github.com>
2019-07-03 02:01:25 +0900

    Delete HP02.js

--- HP02.js
+++
@@ -1,7 +0,0 @@
-var mySwiper = new Swiper ('.swiper-wrapper', {
-  effect: "flip",
-  loop: true,
-  pagination: '.swiper-pagination',
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-})
