a77e74281ad3aa86b071be4d84a537b9763c247b
rasucal <52271061+rasucal@users.noreply.github.com>
2019-07-02 16:02:26 +0900

    Add files via upload

---
+++ HP02.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper ('.swiper-wrapper', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})
